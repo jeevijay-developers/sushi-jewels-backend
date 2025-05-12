c5dd5bb633baf97c7d617b771f08179ccdeb17c7
Abhishek Yadav <monuy8830@gmail.com>
2025-05-12 11:12:15 +0530

    refactor: simplify MongoDB connection options and uncomment seed script functionality

--- script/seed.js
+++ script/seed.js
@@ -1,11 +1,11 @@
-// require("dotenv").config();
-// const { connectDB } = require("../config/db");
+require("dotenv").config();
+const { connectDB } = require("../config/db");
 
-// const Admin = require("../models/Admin");
-// const adminData = require("../utils/admin");
+const Admin = require("../models/Admin");
+const adminData = require("../utils/admin");
 
-// // const Customer = require("../models/Customer");
-// // const customerData = require("../utils/customers");
+// const Customer = require("../models/Customer");
+// const customerData = require("../utils/customers");
 
 // const Coupon = require("../models/Coupon");
 // const couponData = require("../utils/coupon");
@@ -13,8 +13,8 @@
 // const Product = require("../models/Product");
 // const productData = require("../utils/products");
 
-// // const Order = require("../models/Order");
-// // const orderData = require("../utils/orders");
+// const Order = require("../models/Order");
+// const orderData = require("../utils/orders");
 
 // const Category = require("../models/Category");
 // const categoryData = require("../utils/categories");
@@ -28,48 +28,48 @@
 // const Attribute = require("../models/Attribute");
 // const attributeData = require("../utils/attributes");
 
-// const Setting = require("../models/Setting");
-// const settingData = require("../utils/settings");
+const Setting = require("../models/Setting");
+const settingData = require("../utils/settings");
 
-// connectDB();
-// const importData = async () => {
-//   try {
-//     await Language.deleteMany();
-//     await Language.insertMany(languageData);
+connectDB();
+const importData = async () => {
+  try {
+    // await Language.deleteMany();
+    // await Language.insertMany(languageData);
 
-//     await Currency.deleteMany();
-//     await Currency.insertMany(currencyData);
+    // await Currency.deleteMany();
+    // await Currency.insertMany(currencyData);
 
-//     await Attribute.deleteMany();
-//     await Attribute.insertMany(attributeData);
+    // await Attribute.deleteMany();
+    // await Attribute.insertMany(attributeData);
 
-//     // await Customer.deleteMany();
-//     // await Customer.insertMany(customerData);
+    // await Customer.deleteMany();
+    // await Customer.insertMany(customerData);
 
-//     await Admin.deleteMany();
-//     await Admin.insertMany(adminData);
+    await Admin.deleteMany();
+    await Admin.insertMany(adminData);
 
-//     await Category.deleteMany();
-//     await Category.insertMany(categoryData);
+    // await Category.deleteMany();
+    // await Category.insertMany(categoryData);
 
-//     await Product.deleteMany();
-//     // await Product.insertMany(productData);
+    // await Product.deleteMany();
+    // await Product.insertMany(productData);
 
-//     await Coupon.deleteMany();
-//     await Coupon.insertMany(couponData);
+    // await Coupon.deleteMany();
+    // await Coupon.insertMany(couponData);
 
-//     // await Order.deleteMany();
-//     // await Order.insertMany(orderData);
+    // await Order.deleteMany();
+    // await Order.insertMany(orderData);
 
-//     await Setting.deleteMany();
-//     await Setting.insertMany(settingData);
+    await Setting.deleteMany();
+    await Setting.insertMany(settingData);
 
-//     console.log("data inserted successfully!");
-//     process.exit();
-//   } catch (error) {
-//     console.log("error", error);
-//     process.exit(1);
-//   }
-// };
+    console.log("data inserted successfully!");
+    process.exit();
+  } catch (error) {
+    console.log("error", error);
+    process.exit(1);
+  }
+};
 
-// importData();
+importData();
