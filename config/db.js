282df85ee0aac034a33248fcfde4b8a34707c312
Abhishek Yadav <monuy8830@gmail.com>
2025-05-12 10:40:36 +0530

    refactor: update MongoDB connection options and comment out seed script

--- config/db.js
+++ config/db.js
@@ -3,7 +3,12 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI);
+    await mongoose.connect(process.env.MONGO_URI, {
+      useFindAndModify: false,
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+    });
     console.log("mongodb connection success!");
   } catch (err) {
     console.log("mongodb connection failed!", err.message);
